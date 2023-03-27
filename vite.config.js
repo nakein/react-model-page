26c1b3d521fd1739bf1755f1e699b4f7ce21f2c2
nakein <55964645+nakein@users.noreply.github.com>
2023-03-26 21:34:38 -0300

    Ready to production

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     root: "src",
-    assetsInclude: "**/*.JPG"
+    assetsInclude: "**/*.JPG",
+    build: {
+        outDir: '../dist'
+    }
 });
